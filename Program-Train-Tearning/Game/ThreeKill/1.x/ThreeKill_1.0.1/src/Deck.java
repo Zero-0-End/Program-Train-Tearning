256633dce518082bac78bf545cc8e41c7eb1c489
Zero-0-End <git@github.com:Zero-0-End/Learn.git>
2026-01-10 18:50:41 +0800

    1.0.1

---
+++ Program-Train-Tearning/Game/ThreeKill/1.x/ThreeKill_1.0.1/src/Deck.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class Deck {
+    private List<Card> drawPile;
+
+    public Card deal(){
+        return null;
+    }
+}
---
+++ Program-Train-Tearning/Game/ThreeKill/1.x/ThreeKill_1.0.1/src/Game.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class Game {
+    private List<Player> players;
+    private Deck deck;
+
+    public void gameStart(){
+
+    }
+}
