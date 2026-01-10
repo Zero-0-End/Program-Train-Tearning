a0f31bfb1cd92a7becaf5e8b0328a0c5fad43881
Zero-0-End <git@github.com:Zero-0-End/Learn.git>
2026-01-11 00:55:56 +0800

    1
    Signed-off-by: Zero-0-End <git@github.com:Zero-0-End/Learn.git>

--- Program-Train-Tearning/Game/ThreeKill/1.x/ThreeKill_1.0.0/src/Deck.java
+++ Program-Train-Tearning/Game/ThreeKill/1.x/ThreeKill_1.0.0/src/Deck.java
@@ -1,11 +1,17 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Deck {
-    private List<Card> drawPile;
+    private List<Card> drawPile = new ArrayList<>();
 
     public void addCard(Card card){
 
     }
+
+    public boolean isEmpty(){
+        return true;
+    }
+
     public Card deal(){
         return null;
     }
--- Program-Train-Tearning/Game/ThreeKill/1.x/ThreeKill_1.0.0/src/Player.java
+++ Program-Train-Tearning/Game/ThreeKill/1.x/ThreeKill_1.0.0/src/Player.java
@@ -1,8 +1,9 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
     private String name;
-    private List<Card> handCard;
+    private List<Card> handCard = new ArrayList<>();
     private int health;
 
     public void drawCard(){
