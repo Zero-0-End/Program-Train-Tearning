db468205bfd8a10fdc666397e14ea5366d5ea5e2
Zero-0-End <git@github.com:Zero-0-End/Learn.git>
2026-01-13 21:28:04 +0800

    调整结构

--- Program-Train-Tearning/Game/ThreeKill/1.x/ThreeKill_1.2.0/src/Main.java
+++ Program-Train-Tearning/Game/ThreeKill/1.x/ThreeKill_1.2.0/src/Main.java
@@ -1,17 +1,33 @@
+import java.util.List;
+import java.util.Random;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Player p1 = new Player("曹操",4);
+        Player p2 = new Player("刘备",4);
+        Player p3 = new Player("孙权",4);
+        Player p4 = new Player("关羽",4);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Deck deck = new Deck();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        Random random = new Random();
+        for (int i=0;i<20;i++){
+            if (random.nextInt(100)<60){
+                deck.addDrawPile(new ShaCard());
+            } else if (random.nextInt(100)<70) {
+                deck.addDrawPile(new ShanCard());
+            } else if (random.nextInt(100)<80) {
+                deck.addDrawPile(new TaoCard());
+            } else{
+                deck.addDrawPile(new JiuCard());
+            }
         }
+
+        List players = List.of(p1,p2,p3,p4);
+
+        Game game = new Game(players,deck);
+        game.start();
     }
 }
--- Program-Train-Tearning/Game/ThreeKill/1.x/ThreeKill_1.2.0/src/Player.java
+++ Program-Train-Tearning/Game/ThreeKill/1.x/ThreeKill_1.2.0/src/Player.java
@@ -1,2 +1,167 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
 public class Player {
+    private String name;
+    private int health;
+    private int maxHealth;
+    private boolean isDanger;
+    private boolean isAlive;
+    private boolean isDrunk;
+    private List<Card> handCards = new ArrayList<>();
+
+    public Player(String name, int health){
+        this.health = health;
+        this.name = name;
+        this.maxHealth = health;
+        this.isAlive = true;
+    }
+
+
+    //受到伤害
+    //受伤害后血量低于0进入濒死
+    public void takeDamage(int damage){
+        this.health -= damage;
+        System.out.println(this.getName()+"受到"+damage+"点伤害，当前体力值为："+this.health);
+        if (this.health <=0){
+            this.setDanger(true);//进入濒死状态
+        }
+    }
+
+    //自救
+    public void rescueSelf(){
+        Iterator<Card> iterator = handCards.iterator();
+        while(iterator.hasNext()){
+            Card card = iterator.next();
+            if (card instanceof JiuCard || card instanceof TaoCard){
+                System.out.println(this.getName()+"使用"+card.getName()+"回复体力");
+                this.heal(1);
+                iterator.remove();
+                card.use(this,this);
+
+                if (this.health> 0){
+                    this.setDanger(false);//脱离濒死状态
+                    return;
+                }
+            }
+        }
+    }
+
+
+
+    //求援
+    public Class<? extends Card> askForHelp(Class clazz){
+        System.out.println(this.getName()+"请求救援");
+        return clazz;
+    }
+
+    //恢复
+    //不能超过最大体力
+    public void heal(int healNum){
+       if (this.health<this.maxHealth){
+           this.health += healNum;
+       }
+
+       if (this.health>this.maxHealth){
+           this.health = this.maxHealth;
+           System.out.println(this.getName()+"体力已满");
+       }
+    }
+
+    //摸牌
+    public void drawCard(Card card){
+        this.handCards.add(card);
+        System.out.println(this.name+"摸到了 "+card.getName());
+    }
+
+
+
+    //出牌
+    //出牌和弃牌动作分离
+    public void playCard(Card card, Player target){
+        card.use(this, target);
+    }
+
+    //弃牌
+    public void diaCard(Card card){
+        this.handCards.remove(card);
+    }
+
+    //响应
+    //按类型检索手牌
+    public Card  findCardByType(Class<?extends Card> cardTYpe){
+        for (Card card : handCards){
+            if (cardTYpe.isInstance(card)){
+                return card;
+            }
+        }
+        return null;
+    }
+
+    //响应
+    //按名称检索手牌
+    public Card findCardByName(String name){
+        for (Card card : handCards){
+            if (card.getName().equals(name)){
+                return card;
+            }
+        }
+        return null;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public boolean getDanger() {
+        return this.isDanger;
+    }
+
+    public void setDanger(boolean danger) {
+        this.isDanger = danger;
+        if (this.isDanger){
+            System.out.println(this.getName()+"进入【濒死】状态");
+        }else{
+            System.out.println(this.getName()+"脱离【濒死】状态");
+        }
+    }
+
+
+
+    public boolean getAlive() {
+        return this.isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.isAlive = alive;
+        if (alive == false){
+            System.out.println(this.getName()+"已【死亡】出局.");
+        }
+    }
+    public int getHealth(){
+        return this.health;
+    }
+
+    public int getMaxHealth(){
+        return this.maxHealth;
+    }
+
+    public List<Card> getHandCards(){
+        return this.handCards;
+    }
+
+    public boolean getDrunk(){
+        return this.isDrunk;
+    }
+
+    public void setDrunk(boolean Drunk){
+        this.isDrunk = Drunk;
+        if (this.isDrunk){
+            System.out.println(this.getName()+"处于【醉酒】状态");
+        }else{
+            System.out.println(this.getName()+"解除【醉酒】状态");
+        }
+    }
 }
+
